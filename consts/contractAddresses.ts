802b7f0d20dcad15b32104a3da9a746675db1763
TimeTrolls <130114332+TimeTrolls@users.noreply.github.com>
2023-04-06 20:08:10 -0500

    Add files via upload

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x19F9524b97F93C1414c3da218CB63e1426b11a8c";
+export const tokenContractAddress =
+  "0x51f6cDaC8a0fEEc27810C7535829dF6783d95E0f";
+export const stakingContractAddress =
+  "0x2bf1A030577e55f7f13561bf92c6dde9654586FB";
---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      {/* Top Section */}
+      <h1 className={styles.h1}>Time Trolls Staking</h1>
+      <div className={styles.nftBoxGrid}>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake`)}
+        >
+          {/* Staking an NFT */}
+          <Image src="/icons/token.webp" alt="token" width={64} height={64} />
+          <h2 className={styles.selectBoxTitle}>Touchgrass</h2>
+          <p className={styles.selectBoxDescription}>
+            Touching grass turns your NFT to Stone, locking him away and earning you <b>$STONE</b>.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
